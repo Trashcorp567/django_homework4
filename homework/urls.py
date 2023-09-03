c1efc2c86fb968ceab7baff3f1731421546b003e
Trashcorp567 <Trashcorp567@gmail.com>
2023-09-04 02:10:26 +0300

    rework

--- homework/urls.py
+++ homework/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from homework.apps import HomeworkConfig
-from homework.views import index, full_list
+from homework.views import index, full_list, product_detail
 
 app_name = HomeworkConfig.name
 
 urlpatterns = [
     path('', index, name='index'),
     path('full_list/', full_list, name='full_list'),
+    path('product_detail/<int:pk>/', product_detail, name='product_detail'),
 ]
--- homework/views.py
+++ homework/views.py
@@ -18,3 +18,11 @@ def full_list(request):
     }
     return render(request, 'catalog/full_list.html', context)
 
+
+def product_detail(request, pk):
+    category_item = Product.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(pk=pk),
+        'title': f'Товар - {category_item.name}'
+    }
+    return render(request, 'catalog/product_detail.html', context)
